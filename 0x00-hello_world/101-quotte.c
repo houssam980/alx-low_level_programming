95ca3851619e81a8baf11beaef3cf4a103eceb83
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-10 09:17:14 -0700

    write function

---
+++ 0x00-hello_world/101-quotte.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print without printf with write
+ * Return: 1 (fail)
+ */
+
+int main(void)
+{
+
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+write(1, msg, 59);
+
+
+
+return (1);
+
+}
+
