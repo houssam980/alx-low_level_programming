f34d8015fab6d6180e332b7a3f0802067202214a
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-11 05:05:19 -0700

    alphabit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: putchar alphabit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+return (0);
+}
