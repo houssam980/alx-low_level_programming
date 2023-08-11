c2aaccabca8cfc20a8fe4480af8fa6d99ac1af1a
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-11 07:36:18 -0700

    for the base

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: putchar without eq
+ * Return: Always 0 (Success)
+ */
+
+
+
+
+
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
+
