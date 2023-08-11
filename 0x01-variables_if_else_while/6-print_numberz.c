9bcc072c738520249f380e7e4be8212c14ef8631
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-11 08:28:09 -0700

    all changes

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: puts(displaying
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+putchar(48 + i);
+putchar('\n');
+return (0);
+}
+
