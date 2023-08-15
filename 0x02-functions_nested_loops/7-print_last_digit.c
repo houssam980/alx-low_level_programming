37ba570c5b541a0112e9f13938df72a6aad67d12
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-15 08:14:19 -0700

    last alphabet

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_last_digit - print the last alphabet
+* int num: input
+* Return: returns result value (success)
+*/
+
+
+
+
+int print_last_digit(int num)
+{
+int gv;
+if (gv < 0)
+{
+_putchar(-gv + 48);
+return (-gv);
+}
+else
+{
+_putchar(gv + 48);
+return (gv);
+}
