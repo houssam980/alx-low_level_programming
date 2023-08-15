e37c76b7332751b055c20ca8810ed5cf00ea2712
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-15 03:31:46 -0700

    putchar alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+* print_alphabet- print alphabet in lower case
+* Description: discription here
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+int f;
+for (f = 97 ; f <= 122 ; f++)
+{
+putchar(f);
+}
+putchar('\n');
+return (0);
+}
