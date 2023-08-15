51022198c9980c6a255111976d82ef157999aaba
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-15 10:19:05 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,9 +11,9 @@ void jack_bauer(void)
 {
 	int hr, mi;
 
-	for (hr = 0; hr <= 24; hr++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for (mi = 0; mi <= 59; mi++)
+		for (mi = 0; mi <= 60; mi++)
 		{
 			_putchar((hr / 10) + '0');
 			_putchar((hr % 10) + '0');
