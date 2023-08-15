33562b4d9cbc1d7c0a33cee4f8bc71e5baf2b22e
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-15 11:23:38 -0700

    street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,16 +6,12 @@
 void print_to_98(int n)
 {
 	int vn ;
-	if (n >= 98)
-	{
+	if (n > 98)
 		for (vn = n; vn  > 98; vn--)
 		printf("%d, ", vn);
-	}
-
 	else
-	{
 		for (vn = n; vn < 98; vn++)
 		printf("%d, ", n);
 		printf("98\n");
-	}
+	
 }
