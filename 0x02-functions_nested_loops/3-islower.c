6815bc263cd806a149367506ec894165bdcb11bd
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-15 04:34:16 -0700

    check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* int _islower(int c) - check for lowercase
+* Description: discription here
+* Return: return 1 if the c is lowercase 0 otherwise
+*/
+
+
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
