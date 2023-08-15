fe7459180e3bc14b4b38fd5860ad4f2c29c65cbd
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-15 05:56:59 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - print the alphabet in lower
+* Description: discription here
+*@int: input
+* Return: return 0 always (Success)
+*/
+
+
+
+
+
+int _abs(int)
+{
+	if (n < 0)
+		n = (-10) * n;
+	return (n);
+}
