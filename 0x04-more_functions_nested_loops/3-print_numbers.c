2e4a97d6551e424756f8bae8975d91a0fc11ee46
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-17 04:38:45 -0700

    hello

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,10 +10,10 @@
 
 void print_numbers(void)
 {
-	int i;
-for (i = 48; i <= 57; i++)
-	{
-	_putchar(i + 48);
-	_putchar('\n');
-		return (0);
+char i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i);
+_putchar('\n');
+return (0);
 	}
