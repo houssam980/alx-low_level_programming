99c8df3c6caab47ae41f39db68b8162c7d734d89
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-16 22:15:46 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+* mul - multuple of a and b
+* @a: input a
+* @b: input b
+* Return: return a and b multiplaction (Success)
+*/
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
