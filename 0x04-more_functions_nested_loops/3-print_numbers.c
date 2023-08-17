c35e1207f3420bc7f78beba53fda9df14220de1a
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-16 22:29:52 -0700

    putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers using _putchar
+* Return: return 0 (Success)
+*/
+
+
+
+
+void print_numbers(void)
+{
+	int i;
+for (i = 48; i <= 57; i++)
+	{
+	_putchar(i + 48);
+	_putchar('\n');
+		return (0);
+	}
