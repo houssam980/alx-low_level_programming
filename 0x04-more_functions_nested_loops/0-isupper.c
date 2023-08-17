dc5b569f995427d419ab7cba08cd32e8a505406d
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-16 21:53:54 -0700

    check upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main"
+/**
+* _isupper - check alphabet uppercase
+* @c: input
+* Return: return 1 if upper otherwise Always 0 (Success)
+*/
+
+
+
+
+
+int _isupper(int c)
+{
+	char lt = 'C';
+
+if (isupper(lt)){
+
+return (1);
+}
+else
+{
+return (0);
+}
+}
