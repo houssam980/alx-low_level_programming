c06aac15f237bd70ba1f9afe50795425c185c14e
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-17 11:38:02 -0700

    sqr

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print sqaure using _putchar
+ * @size: size of square
+ * Return: no return
+ */
+void print_square(int size)
+{
+int t_head, t_foot;
+t_foot = 0;
+if (size < 1)
+_putchar('\n');
+while (t_foot < size)
+{
+t_head= 0;
+while (t_head < size)
+{
+_putchar('#');
+t_head++;
+}
+_putchar('\n');
+t_foot++;
+}
+}
+
