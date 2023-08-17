9930eacfe6cac1682da2d537df7bc3514e1d801e
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-17 05:23:20 -0700

    push

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,14 @@
 */
 
 
-
-
-
 void print_most_numbers(void)
 {
-char i;
-for (i >= '0'; i <= '9'; i++)
+char i = 0;
+while (i <= '9')
+{
 if (i != '2' && i != '4')
 _putchar(i);
+}
 _putchar('\n');
 return (0);
 }
