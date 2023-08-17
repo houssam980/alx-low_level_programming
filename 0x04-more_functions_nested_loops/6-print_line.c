4c4ca106b56174dbba0b7cff3e225859a616d0ed
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-17 10:48:48 -0700

    hello

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main"
+
+
+
+/**
+ * print_line - draw a straight line
+ * @n: number of times underscore is printed
+ * Description: Can only use _putchar to print
+ */
+void print_line(int n)
+{
+int printer;
+printer = 0;
+while (printer < n)
+{
+_putchar('_');
+printer;
+}
+_putchar('\n');
+}
+
+
