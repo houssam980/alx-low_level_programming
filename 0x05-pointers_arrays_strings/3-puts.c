4daac32c7a693b8f322041bbb5186fa4cdaa0848
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-21 06:04:50 -0700

    str print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print to stdout
+ * @str: pointer
+ */
+
+
+void _puts(char *str)
+{
+int lp;
+lp = 0;
+while (str[lp] != '\0')
+{
+i++;
+_putchar(str[lp]);
+}
+return (lp);
+}
