dbe6f97c8ea9f9004dea2c3e9245c215363ca2fe
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-21 05:13:37 -0700

    swap it out

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps 
+ * @a: pointer one
+ * @b: pointer two
+ */
+
+
+void swap_int(int *a, int *b)
+{
+int = holder;
+holder = *a ;
+*a = *b;
+*b = holder;
+}
