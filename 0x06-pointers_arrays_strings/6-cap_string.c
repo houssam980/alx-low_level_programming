23dbd67e598192d4607b7620f9b20422df29fee2
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-24 17:22:46 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,33 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * cap_string - capitalize all string's words
- * @entry: input
- * Return: capitalized strings
+ * cap_string - capitalize a string
+ * @entry: This is the input string
+ * Return: String capitalized 
  */
 char *cap_string(char *entry)
 {
-int ch, inpt, lp;
+	int conversion, index, count;
 
-char all[] = {' ', ',', ';', '.', '!','?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-ch = 32;
-for (inpt = 0; entry[inpt] != '\0'; inpt++)
-{
-if (entry[inpt] >= 'inpt' && entry[inpt] <= 'z')
-{
-entry[inpt] =  entry[inpt] - ch;
-}
-ch = 0;
-for (lp = 0; all[lp] != '\0'; lp++)
-{
-if (all[lp] == entry[inpt])
-{
-ch = 32;
-break;
-}
-}
-}
-return (entry);
+	char chars[] = {' ', ',', ';', '.', '!',
+			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+	conversion = 32;
+
+	for (index = 0; entry[index] != '\0'; index++)
+	{
+		if (entry[index] >= 'index' && entry[index] <= 'z')
+		{
+			entry[index] =  entry[index] - conversion;
+		}
+		conversion = 0;
+		for (count = 0; chars[count] != '\0'; count++)
+		{
+			if (chars[count] == entry[index])
+			{
+				conversion = 32;
+				break;
+			}
+		}
+	}
+	return (entry);
 }
