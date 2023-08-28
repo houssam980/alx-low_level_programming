9cd644b5ac986648801d5ce0c87b165c1ba878e3
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-28 05:57:02 -0700

    prfx

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - finds length of prefix substring
+ * @s: string 
+ * @accept: substring of chars
+ * Return: returns n 
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int lp;
+int ln = 0;
+int prx;
+while (*s)
+	{
+lp = 0;
+prx = ln;
+while (*(accept + lp) != '\0')
+{
+if (*(accept + lp) == *s)
+ln++;
+lp++;
+}
+if (prx == ln) 
+break;
+s++;
+}
+return (ln);
+}
