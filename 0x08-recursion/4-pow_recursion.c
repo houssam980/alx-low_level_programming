1846fd416e66d1dcd38683ae8b58f5c4031d66df
houssam980 <houssam.ghmari1243@gmail.com>
2023-08-30 15:31:54 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _pow_recursion - power raised to y
- * @x: Number 
- * @y: Power number 
+ * @x: Number
+ * @y: Power number
  * Return: if y is lower than 0 return -1
  */
 int _pow_recursion(int x, int y)
