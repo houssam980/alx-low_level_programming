4fcbd5f5834426ab008a2d2f31e859fa401f4916
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-01 08:36:05 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry / multiplies two numbers
+ * @argc: arguments
+ * @argv: array with the arguments
+ * Return: always 0
+ **/
+int main(int argc, char *argv[])
+{
+int mul = 0;
+char handler[] = "Error";
+if (argc != 3)
+{
+printf("%s\n", handler);
+return (1);
+}
+mul = asy(argv[1]) * asy(argv[2]);
+printf("%i\n", mul);
+return (0);
+}
