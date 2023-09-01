d020ed527431483a95a220b596c360523fdb6372
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-01 08:24:58 -0700

    passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point / number of arguments passed into it
+ * @argv: array with the arguments
+ * @argv: array with the arguments
+ * Return: always 0
+ **/
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
