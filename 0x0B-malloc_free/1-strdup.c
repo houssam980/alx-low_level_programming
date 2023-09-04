6610d8a5f3afc1aa32c5453484a61b4058b07b97
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-04 11:55:40 -0700

    allocate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,16 +13,17 @@ char *_strdup(char *str)
 {
 	int lp = 0, w = 0;
 	char *p;
-	if (str == NULL)
+if (str == NULL)
 		return (NULL);
 	for (; str[w] != '\0'; w++)
 		;
 	p = malloc(w * sizeof(*str) + 1);
-	if (p == 0)
-		return(NULL);
+	if (p == 0){
+		return (NULL);
+	}
 	else
 	{
-		for(; lp < w; lp++)
+		for (; lp < w; lp++)
 			p[lp] = str[lp];
 	}
 	return (p);
