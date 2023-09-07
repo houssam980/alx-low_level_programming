7e1e6fb775bf69ba3c0b427a9490851a3d9375b2
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-07 06:43:12 -0700

    check malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: bytes.
+ * Return: pointer to memo place 
+ */
+void *malloc_checked(unsigned int b)
+{
+char *poi;
+poi = malloc(b);
+if (poi == NULL)
+exit(98);
+return (poi);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+hello im more malloc free readme
