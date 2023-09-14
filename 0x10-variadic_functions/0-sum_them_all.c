1e0f93abe9f1758865aaf2ef7b5623e6b1189a4f
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-14 07:22:22 -0700

    ps all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,16 +4,18 @@
 /**
  * sum_them_all - sums all arguments passed
  * @n: num
+ *...: int
  * Return: sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
-int sum_of;
-unsigned int lp;
-va_list list;
-va_start(list, n);
-for (lp = 0, sum_of = 0; lp < n; lp++)
-sum_of += va_arg(list, int);
-va_end(list);
+int sum_of = 0, lp = n;
+va_list ap;
+if (!n)
+return (0);
+va_start(ap, n);
+while (lp--)
+sum_of += va_arg(ap, int);
+va_end(ap);
 return (sum_of);
 }
