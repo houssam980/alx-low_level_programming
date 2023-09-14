6f711e16ca2bcb2dedfdd1344b4624e423245c79
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-14 14:56:01 -0700

    anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,87 +1,48 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * op_c - Print character
- * @form: name va_list
- * Return: Nothing.
- */
-void op_c(va_list form)
-{
-printf("%c", va_arg(form, int));
-}
-/**
- * op_i - Print Integer
- * @form: name va_list
- * Return: Nothing.
- */
-
-void op_i(va_list form)
-{
-printf("%i", va_arg(form, int));
-}
-/**
- * op_f - print FLoat numbers
- * @form: name of va_list
- * Return: Nothing.
- */
-
-void op_f(va_list form)
-{
-printf("%f", va_arg(form, double));
-}
-/**
- * op_s -print string
- * @form: name va_list
- * Return: Nothing.
- */
-
-void op_s(va_list form)
-{
-char *str;
-str = va_arg(form, char *);
-if (str == NULL)
-{
-printf("(nil)");
-return;
-}
-printf("%s", str);
-}
-
-/**
- * print_all - check the code for Holberton School students.
- * @format: number of arguments in character format
- * Return: Nothing.
- */
-
+  4  * op_c - Print character
+  5  * @form: name va_list
+  6  * Return: Nothing.
+  7  */
 void print_all(const char * const format, ...)
 {
-va_list all;
-unsigned int lp_1, lp_2;
-char *separator = "";
-f ops[] = {
-{"c", op_c},
-{"i", op_i},
-{"f", op_f},
-{"s", op_s},
-};
-va_start(all, format);
-lp_1 = 0;
-while (format && format[lp_1])
+va_list valist;
+int n = 0, i = 0;
+char *sep = ", ";
+char *str;
+va_start(valist, format);
+
+while (format && format[i])
+i++;
+
+while (format && format[n])
 {
-lp_2 = 0;
-while (lp_2 < 4)
+if (n  == (i - 1))
 {
-if (ops[lp_2].op[0] == format[lp_1])
+sep = "";
+}
+switch (format[n])
 {
-printf("%s", separator);
-separator = ", ";
-ops[lp_2].f(all);
+case 'c':
+printf("%c%s", va_arg(valist, int), sep);
+break;
+case 'i':
+printf("%d%s", va_arg(valist, int), sep);
+break;
+case 'f':
+printf("%f%s", va_arg(valist, double), sep);
+break;
+case 's':
+str = va_arg(valist, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s%s", str, sep);
 break;
 }
-lp_2++;
-}
-lp_1++;
+n++;
 }
 printf("\n");
-va_end(all);
+va_end(valist);
 }
