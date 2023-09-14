61314c27314386b5990c64986d9e61f83b799987
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-14 07:59:03 -0700

    sep

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 
+
 /**
  * print_numbers - prints integer arguments with a separator
  * @separator: - print among the nums
@@ -8,18 +9,17 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-char *com;
-unsigned int lp;
-va_list list;
-if (separator == NULL || *separator == 0)
-com = "";
-else
-com = (char *) separator;
-va_start(list, n);
-if (n > 0)
-printf("%d", va_arg(list, int));
-for (lp = 1; lp < n; lp++)
-printf("%s%d", com, va_arg(list, int));
-printf("\n");
-va_end(list);
+int lp = n;
+va_list ap;
+if (!n)
+{
+    
+    printf("\n");
+    return;
+}
+va_start(ap, n);
+while (lp--)
+    printf("%d%s", va_arg(ap, int), lp ? (separator ? separator : "") : "\n");
+va_end(ap);
+
 }
