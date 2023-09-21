c4bb8c230504f3458d2fa55f182fe63eafe3f9c1
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-21 13:15:09 -0700

    calcul

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+
+
 /**
  * add_node - new node at the beginning of list
  * @head: address
@@ -8,20 +10,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *nuv_head = malloc(sizeof(list_t));
-if (!head || !nuv_head)
+list_t *nuv;
+nuv = malloc(sizeof(list_t));
+if (!nuv)
 return (NULL);
-if (str)
-{
-nuv_head->str = strdup(str);
-if (!nuv_head->str)
-{
-free(nuv_head);
-return (NULL);
+nuv->str = strdup(str);
+nuv->len = _strlen(str);
+nuv->next = *head;
+*head = nuv;
+return (nuv);
 }
-nuv_head->len = _strlen(nuv_head->str);
-}
-nuv_head->next = *head;
-*head = nuv_head;
-return (nuv_head);
+/**
+ * _strlen - Calculate op
+ * @s: str
+ * Return: lenght of the string s.
+ */
+int _strlen(const char *s)
+{
+int index = 0;
+for (; s[index]; index++)
+;
+return (index);
 }
