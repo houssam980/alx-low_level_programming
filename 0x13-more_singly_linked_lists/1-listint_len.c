f614b9560a9d1450fd2b755859819b4fd7e32800
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-26 06:43:35 -0700

    lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+
+/**
+ * print_listint - print all list elements
+ * @h:pointer / list
+ * Return: nodes number
+ **/
+size_t print_listint(const listint_t *h)
+{
+const listint_t *nd = h;
+size_t lp = 0;
+while (nd)
+{
+lp++;
+nd = nd->next;
+}
+return (lp);
+}
