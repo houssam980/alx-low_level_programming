2bd1180d412fed53964d7b51253023b18d134b41
houssam980 <houssam.ghmari1243@gmail.com>
2023-09-28 13:02:15 -0700

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,13 +7,13 @@
  * @index: unsigned int
  * Return: 1 if secce if error -1
  */
- 
 
- int clear_bit(unsigned long int *n, unsigned int index)
- {
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
 if (index >= sizeof(n) * 8)
 return (-1);
 if (*n & 1L << index)
 *n ^= 1L << index;
 return (1);
- }
+}
