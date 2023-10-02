51a108db4f570c7d81da10024fb6c79ac30a1f4e
houssam980 <houssam.ghmari1243@gmail.com>
2023-10-02 09:17:57 -0700

    file io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 
-
+#define READ_BUF_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
