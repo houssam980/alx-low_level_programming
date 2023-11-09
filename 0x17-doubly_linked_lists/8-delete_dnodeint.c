f469034d0d0bf91b4af6f48831d60257440bcef8
houssam980 <houssam.ghmari1243@gmail.com>
2023-11-09 12:43:53 -0800

    delete node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -6,38 +6,37 @@
 * @index: index to delete
 * Return: return -1 if Error
 */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *hd_1;
-dlistint_t *hd_2;
-int ctr;
-hd_1 = *head;
-if (hd_1 != NULL)
-while (hd_1->prev != NULL)
-hd_1 = hd_1->prev;
+dlistint_t *head1;
+dlistint_t *head2;
+unsigned int ctr;
+head1 = *head;
+if (head1 != NULL)
+while (head1->prev != NULL)
+head1 = head1->prev;
 ctr = 0;
-while (hd_1 != NULL)
+while (head1 != NULL)
 {
 if (ctr == index)
 {
 if (ctr == 0)
 {
-*head = hd_1->next;
+*head = head1->next;
 if (*head != NULL)
 (*head)->prev = NULL;
 }
 else
 {
-hd_2->next = hd_1->next;
-if (hd_1->next != NULL)
-hd_1->next->prev = hd_2;
+head2->next = head1->next;
+if (head1->next != NULL)
+head1->next->prev = head2;
 }
-free(hd_1);
+free(head1);
 return (1);
 }
-hd_2 = hd_1;
-hd_1 = hd_1->next;
+head2 = head1;
+head1 = head1->next;
 ctr++;
 }
 return (-1);
