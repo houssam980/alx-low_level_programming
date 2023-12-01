4fe10ac2ea10ee31f6ff1bd8265f72600922fa09
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-01 06:16:38 -0800

    index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get the index.
+ * @key: key
+ * @size: size
+ * Return: The index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
+}
