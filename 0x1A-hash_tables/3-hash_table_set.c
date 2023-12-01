96c1c0b1dbe0ae7096cf843fe73e337867371b7e
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-01 06:29:48 -0800

    set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,21 +9,14 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
-
-
 hash_node_t *nw;
 unsigned long int idx, lp;
 char *val_cp;
-
-
 if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 return (0);
-
 val_cp = strdup(value);
 if (val_cp == NULL)
 return (0);
-
 idx = key_index((const unsigned char *)key, ht->size);
 for (lp = idx; ht->array[lp]; lp++)
 {
@@ -34,7 +27,6 @@ ht->array[lp]->value = val_cp;
 return (1);
 }
 }
-
 nw = malloc(sizeof(hash_node_t));
 if (nw == NULL)
 {
@@ -50,8 +42,5 @@ return (0);
 nw->value = val_cp;
 nw->next = ht->array[idx];
 ht->array[idx] = nw;
-
-
-
 return (1);
 }
