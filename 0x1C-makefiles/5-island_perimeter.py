71791515ac9b113d9d15ce152c2e921e6848612f
houssam980 <houssam.ghmari1243@gmail.com>
2024-01-31 06:28:36 -0800

    py one

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,11 +13,11 @@ def island_perimeter(grid):
         for x, ce in enumerate(rw):
             if ce == 1:
                 if y == 0 or grid[y - 1][x] == 0:
-                    ret += 1
+                    res += 1
                 if y == len(grid) - 1 or grid[y + 1][x] == 0:
-                    ret += 1
+                    res += 1
                 if x == 0 or grid[y][x - 1] == 0:
-                    ret += 1
+                    res += 1
                 if x ==len(rw) - 1 or grid[y][x + 1] == 0:
                     res += 1
-    return res	
+    return res
