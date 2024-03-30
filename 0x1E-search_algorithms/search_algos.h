2ddcacabf1bb7357915976c76b6663d57a6ad81e
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-30 16:47:05 +0000

    fixed

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -38,5 +38,6 @@ struct skipl_s *express;
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
